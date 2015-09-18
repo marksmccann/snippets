26bef8e2a34b98e2b75cf19d98bfb8c9a2be189c
Mark McCann <mail@markmccann.me>
2015-09-18 15:57:22 -0500

    Create dom.js

---
+++ js/dom.js
@@ -0,0 +1,98 @@
+// -----------------------------------------------------------------
+// DOM Traversal and Manipulation
+// @author: Mark McCann (www.markmccann.me)
+// @license: None, Public Domain
+// -----------------------------------------------------------------
+
+/**
+ * has class
+ * @param {object} target
+ * @param {string} className
+ * @return {boolean} 
+ */
+var hasClass = function( target, className ) {
+    return target.className.match(new RegExp('(\\s|^)'+className+'(\\s|$)'));
+}
+
+/**
+ * add class
+ * @param {object/array} target
+ * @param {string} className
+ * @require hasClass()
+ */
+var addClass = function( target, className ) {
+    if( ! hasClass(target, className) ) {
+        target.className += target.className.length == 0 ? className : " " + className;
+    }
+}
+
+/**
+ * remove class
+ * @param {object/array} target
+ * @param {string} className
+ * @require hasClass()
+ */
+var removeClass = function( target, className ) {
+    if( hasClass(target, className) ) {
+        var re = new RegExp("(\\s|^)" + className + "(\\s|$)", "g");
+        target.className = target.className.replace(re , '');
+    }
+}
+
+/**
+ * toggle a class
+ * @param {object} target
+ * @param {string} className
+ * @require hasClass(), addClass(), removeClass()
+ */
+var toggleClass = function( target, className ) {
+    hasClass(target,className) ? removeClass(target,className) : addClass(target,className);
+}
+
+// IE10+ Versions:
+document.getElementById("MyElement").classList.contains('class');
+document.getElementById("MyElement").classList.remove('class');
+document.getElementById("MyElement").classList.toggle('class');
+document.getElementById("MyElement").classList.add('class');
+
+/**
+ * inserts element after target element
+ * @param  {dom object} newElement
+ * @param  {dom object} target
+ */
+var insertAfter = function ( newElement, target ) {
+    var parent = target.parentNode;
+    if( parent.lastchild == target ) {
+        parent.appendChild( newElement );
+    } else {
+        parent.insertBefore( newElement, target.nextSibling );
+    }
+    element.parentNode.replaceChild( container, element );
+}
+ 
+/**
+ * replaces node
+ * @param  {dom object} newElement
+ * @param  {dom object} target
+ */
+var replace = function( newElement, target ) {
+    target.parentNode.replaceChild( newElement, target );
+}
+
+/**
+ * removes attribute
+ * @param  {dom object} target
+ * @param  {string} attr
+ */
+var removeAttr = function ( target, attr ) {
+    if ( target.hasAttribute( attr ) ) target.removeAttribute( attr );
+},
+
+/**
+ * retrieves value
+ * @param  {dom object} target
+ * @param  {string} attr
+ */
+var getAttr = function ( target, attr) {
+    if ( target.hasAttribute( attr ) ) return target.getAttribute( attr );
+},
