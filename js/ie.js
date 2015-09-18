b19d84956bcafa2b775cab3230cae5da7c1dc208
Mark McCann <mail@markmccann.me>
2015-09-18 16:06:33 -0500

    Update and rename events.js to ie.js

--- js/ie.js
+++ js/ie.js
@@ -1,11 +1,11 @@
 // ---------------------------------------------------------------
-// Event Handling 
+// IE-friendly JS
 // @Author: Mark McCann
 // @License: None, Public Domain
 // ---------------------------------------------------------------
  
 /**
- * a browser-friendly way to add events
+ * add event
  * @param {object} target - element receiving event
  * @param {string} event - event type
  * @param {function} fn - callback
@@ -21,7 +21,7 @@ var addEvent = function( target, event, fn ) {
 }
 
 /**
- * a browser-friendly way to remove events
+ * remove events
  * @param {object} target - element losing event
  * @param {string} event - event type
  * @param {function} fn - callback
@@ -36,8 +36,40 @@ var removeEvent = function( target, event, fn ) {
 }
 
 /**
- * a borwser-friendly way to prevent event defaults
+ * prevent default
  */
 var preventDefault = function(e) {
     e.preventDefault ? e.preventDefault() : e.returnValue = false;
+}
+
+/**
+ * ajax
+ * @param {string} method - request type, get or post
+ * @param {string} url - url to be called
+ * @param {boolean} async - asynchronous call or not
+ */
+var ajax = function ( method, url, async ) {
+    var xmlhttp;
+    if ( window.XMLHttpRequest ) {
+      xmlhttp = new XMLHttpRequest();
+    } else {
+      xmlhttp = new ActiveXObject( "Microsoft.XMLHTTP" );
+    }
+    xmlhttp.onreadystatechange = function () {
+      if ( xmlhttp.readyState == 4 && xmlhttp.status == 200 ) {
+        var response = JSON.parse( xmlhttp.responseText );  
+        // do something ...
+      }
+    }
+    xmlhttp.open( method, url, async);
+    xmlhttp.send();
+}
+
+/**
+ * determines if browser is IE and which version
+ * @return {int,false}
+ */
+var isIE = function () {
+    var myNav = navigator.userAgent.toLowerCase();
+    return ( myNav.indexOf('msie') != -1 ) ? parseInt( myNav.split('msie')[1] ) : false;
 }
